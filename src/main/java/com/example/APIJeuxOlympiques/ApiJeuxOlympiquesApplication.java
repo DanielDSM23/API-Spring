acd74c65e3039208d302332bf5e7c31cac986dfa
Mathis Boulais <https://github.com/Asukyy>
2024-04-21 10:24:33 +0200

    init

---
+++ src/main/java/com/example/APIJeuxOlympiques/ApiJeuxOlympiquesApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIJeuxOlympiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJeuxOlympiquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJeuxOlympiquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APIJeuxOlympiques/ApiJeuxOlympiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIJeuxOlympiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJeuxOlympiquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
